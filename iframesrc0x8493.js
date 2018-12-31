2b3f5bb68e5674eb072cf38707928eaae87a5244
Schober, George Adam <gaschober@wowway.com>
2018-12-30 23:40:33 -0500

    rebuild

--- iframesrc0x8493.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    document.getElementById('iSlalom').src = 'https://www.youtube.com/embed/7Necm9_6wrs';
-    document.getElementById('iTrick').src = 'https://www.youtube.com/embed/3Og3B_E2HrY';
-    document.getElementById('iJump').src = 'https://www.youtube.com/embed/t4ZGKI8vpcg';
-    document.getElementById('iMSU').src = 'https://www.youtube.com/embed/lrewJcxQeSk';
- });
